757545705bb15cbffb94877dd85bbd071c2f9f52
Noemi Lasalandra <nlasalandra@collaborative.org>
2022-12-13 15:37:29 -0500

    chore: init

---
+++ README.md
@@ -0,0 +1,5 @@
+# SEIS Admin and Data Guide
+
+This repository contains both the [templated code](#source-files) and the [fully built site](#compiled-site) for CES's SEIS Aspen Admin and Data Guide.
+
+If you are trying to *view* the SEIS Aspen Admin and Data Guide and somehow wound up here, please visit: https://ces-seis.github.io/aspen-admin-guide/.
---
+++ _config.ts
@@ -0,0 +1,24 @@
+import lume from "lume/mod.ts";
+import resolveUrls from "lume/plugins/resolve_urls.ts";
+import basePath from "lume/plugins/base_path.ts";
+
+const gh_org = "ces-seis";
+const gh_repo = "aspen-admin-guide";
+const site_url = "https://" + gh_org + ".github.io/" + gh_repo;
+
+const site = lume({
+    src: ".",
+    // We build the site out-of-tree to avoid accidentally
+    // checking in the docs folder to main.
+    dest: "../gh-pages/docs",
+    location: new URL(site_url),
+});
+
+site.ignore("README.md");
+
+site.copy("assets", "assets");
+
+site.use(basePath());
+site.use(resolveUrls());
+
+export default site;
